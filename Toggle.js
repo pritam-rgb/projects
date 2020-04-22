c856a38fa2d963fa4269123325d04051d39c8d38
Pritam Chatterjee <63921092+pritam-rgb@users.noreply.github.com>
2020-04-22 16:32:27 +0530

    Add files via upload

---
+++ Toggle.js
@@ -0,0 +1,11 @@
+
+$("#button").on('click',function(){
+	
+	$('body').css('background-color','black')
+	$('h1').css('color','white')
+	$('#Toggle').css('background-color','white')
+	$('#button').css('position','relative')
+	$('#button').css('right','-90%')
+
+
+});
